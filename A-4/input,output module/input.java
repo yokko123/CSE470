94eec77a7896b16b94063a78f075753a81526e4e
Md Sazidur Rahman <mdsazidur02@gmail.com>
2020-02-20 14:37:47 +0600

    input output module

---
+++ A-4/input,output module/input.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package runner;
+import java.util.*;
+public class input {
+    public int inp(){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Please enter a number");
+    int a = sc.nextInt();
+    return a;
+}
+}
